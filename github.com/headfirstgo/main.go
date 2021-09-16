29274ce37c39cc44c140d53d46024447e08e8c76
lazycruise <gatesworkline@gmail.com>
2021-09-16 19:24:32 +0100

    update subscriber and address type
    linking a defined struct type into another struct type

--- github.com/headfirstgo/main.go
+++ github.com/headfirstgo/main.go
@@ -24,4 +24,29 @@ func main() {
 	employee.Salary = 60000
 	fmt.Println(employee.Name)
 	fmt.Println(employee.Salary)
+
+	// testing the declaration of Address type
+	var address magazine.Address
+	address.City = "Ile-Ife"
+	address.PostalCode = "220005"
+	address.State = "Osun"
+	address.Street = "OAU SSQ"
+	fmt.Println(address)
+
+	// initializing Subscriber by adding existing HomeAddress
+	subscriber2 := magazine.Subscriber{Name: "akinlua olorunfemi", Rate: 3.99, Active: true}
+	subscriber2.HomeAddress = address
+	fmt.Println(subscriber2)
+
+	// initializing Subscriber by default
+	subscriber3 := magazine.Subscriber{Name: "akinlua olorunfemi", Rate: 4.99, Active: true, HomeAddress: magazine.Address{Street: "lukman", City: "lagos"}}
+	fmt.Println(subscriber3)
+
+	// testing
+	fmt.Println(subscriber3.HomeAddress.City)
+
+	// assigning new values to subscriber variable
+	subscriber.HomeAddress.City = "san francisco"
+	subscriber.HomeAddress = magazine.Address{PostalCode: "68111"} // overwrites the previous line of assignment
+	fmt.Println(subscriber.HomeAddress)
 }
